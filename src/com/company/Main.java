2d18fd6d9bcdaa63190184d7b0473d4cbd0b379c
Tanveer Hehar <tanveer.hehar@sjsu.edu>
2019-09-10 16:33:57 -0700

    Assignment1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+ 
+public class Main {
+
+    public static void main(String[] args) {
+	   System.out.println("   ()  () ");
+	   System.out.println("  (  ^^  )  ");
+       System.out.println("   (  ~ )  < Hello 49 J ");
+       System.out.println("     ---      My name is TJ");
+    }
+
+    }
